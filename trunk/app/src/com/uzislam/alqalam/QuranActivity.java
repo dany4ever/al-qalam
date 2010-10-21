6112ed327251990ee642c1ed332d2d2fd4f300d4
uzbmaster <uzbmaster@fa1aa23a-9845-11de-bc73-0172961ec0a2>
2010-10-21 02:21:36 +0000

    Impelement data pack installer routine

--- trunk/app/src/com/uzislam/alqalam/QuranActivity.java
+++ trunk/app/src/com/uzislam/alqalam/QuranActivity.java
@@ -3,10 +3,17 @@ package com.uzislam.alqalam;
 import java.io.File;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,6 +24,9 @@ public class QuranActivity extends Activity {
 	private static String[] 	SurahTitles;
 	private static boolean[]	SurahIsDownloaded;
 	//private static boolean[]	SurahIsAudioDownloaded;
+	private static final String ARABIC_DATA_PACK_INSTALLER_MARKET_LINK =
+		"market://search?q=pname:com.uzislam.alqalam.arabicinstaller";
+	private AlertDialog mAlertDialog;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,11 +65,21 @@ public class QuranActivity extends Activity {
 						startActivity(quranIntent);
 					} 
 					else {
-						// TODO: request file download, and perform download
+						displayNoArabicPack();
 					}
 			}
         });
  	}
+	
+	@Override
+    protected void onStop() {
+        super.onStop();
+        // TODO: Find better implementation to handle Window Leak bug
+        if (mAlertDialog != null) {
+            mAlertDialog.dismiss();
+            mAlertDialog = null;
+        }
+    }
 	
 	private void checkDownloadedSurahs() {
 		String fileName;//, surahNumber, AyatNumber;
@@ -75,5 +95,39 @@ public class QuranActivity extends Activity {
 		  File file = new File(_file);
 		  return file.exists();
 	}
+	
+	private void displayNoArabicPack() {
+        try {
+            PackageManager pm = getPackageManager();
+            @SuppressWarnings("unused")
+			ApplicationInfo info = pm.getApplicationInfo("com.android.vending", 0);
+        } catch (NameNotFoundException e) {
+            // The user does not have Android Market
+        	Toast.makeText(this, R.string.android_maket_app_missing, Toast.LENGTH_LONG);
+            return;
+        }
+
+        AlertDialog.Builder noArabicPackAlert = new AlertDialog.Builder(this);
+        noArabicPackAlert.setTitle(R.string.no_arabic_pack_alert_title);
+        noArabicPackAlert.setMessage(R.string.no_arabic_pack_alert_message);
+
+        noArabicPackAlert.setPositiveButton(android.R.string.ok,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                	Intent marketIntent = new Intent(Intent.ACTION_VIEW,
+                			Uri.parse(ARABIC_DATA_PACK_INSTALLER_MARKET_LINK));
+                    startActivity(marketIntent);
+                }
+            });
+
+        noArabicPackAlert.setNegativeButton(android.R.string.cancel,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+
+        mAlertDialog = noArabicPackAlert.create();
+        mAlertDialog.show();
+    }
 
 }
