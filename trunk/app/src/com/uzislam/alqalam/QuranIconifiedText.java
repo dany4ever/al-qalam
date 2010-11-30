5cabcc3b4a7b1fb56adf73f9102d7b9f8fadc65e
uzbmaster <uzbmaster@fa1aa23a-9845-11de-bc73-0172961ec0a2>
2010-11-30 11:39:06 +0000

    Added LGPL text

--- trunk/app/src/com/uzislam/alqalam/QuranIconifiedText.java
+++ trunk/app/src/com/uzislam/alqalam/QuranIconifiedText.java
@@ -1,23 +1,23 @@
-package com.uzislam.alqalam;
-
-/* 
+/*
  * Copyright 2010 (c) Al-Qalam Project
- *  
+ *
  * This file is part of Al-Qalam (com.uzislam.alqalam) package.
- * 
+ *
  * Al-Qalam is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as 
  * published by the Free Software Foundation, either version 3 of the License, 
  * or (at your option) any later version.
- * 
+ *
  * Al-Qalam is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
+package com.uzislam.alqalam;
 
 import android.graphics.drawable.Drawable;
 
