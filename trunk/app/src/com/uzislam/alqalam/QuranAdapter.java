b68eabac3d5306dced3b97a3b428b918cb8c79e0
elmurod <elmurod@fa1aa23a-9845-11de-bc73-0172961ec0a2>
2010-11-30 10:26:44 +0000

    Ogranize packaging, and add License header to files

--- trunk/app/src/com/uzislam/alqalam/QuranAdapter.java
+++ trunk/app/src/com/uzislam/alqalam/QuranAdapter.java
@@ -1,5 +1,24 @@
 package com.uzislam.alqalam;
 
+/* 
+ * Copyright 2010 (c) Shuhrat Dehkanov and Elmurod Talipov
+ *  
+ * This file is part of Al-Qalam (com.uzislam.alqalam package).
+ * 
+ * Al-Qalam  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Al-Qalam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 import android.content.Context;
